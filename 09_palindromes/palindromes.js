fcb3ea95a2360b941e9d82f55fadbebfa3e50056
robeanobug <fakeemail@gmail.com>
2025-04-21 17:10:32 -0400

    completed 09 palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringArr = string
+        .toLowerCase()
+        .split('')
+        .filter(char => /\w/.test(char));
+    const stringArrFlip = [...stringArr].reverse();
 
+    return stringArr.length === stringArrFlip.length &&
+        stringArr.every((letter, index) => letter === stringArrFlip[index]);
 };
 
 // Do not edit below this line
