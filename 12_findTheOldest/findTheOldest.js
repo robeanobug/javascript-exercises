a82242f17ffedafaabe335557bcc271b5f1fef99
robeanobug <fakeemail@gmail.com>
2025-04-22 22:52:57 -0400

    completed 12 findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(person => 
+        (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth);
+    return people.find(person => ((person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth) ===Math.max(...ages));
 };
 
 // Do not edit below this line
